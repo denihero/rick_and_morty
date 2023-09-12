43d58aaae76baca0bb167632f6da4ad761d0da0d
denihero <denihero81@gmail.com>
2023-09-13 02:35:18 +0400

    add filter

--- lib/feature/characters/presentation/widget/character_filter.dart
+++ lib/feature/characters/presentation/widget/character_filter.dart
@@ -2,6 +2,7 @@ import 'package:auto_route/auto_route.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:rich_and_morti_test_task/core/widget/primary_button.dart';
 import 'package:rich_and_morti_test_task/feature/characters/presentation/bloc/character_bloc.dart';
 
 class CharacterFilter extends StatefulWidget {
@@ -18,6 +19,38 @@ class _CharacterFilterState extends State<CharacterFilter> {
   List<String> gender = ['Female', 'Male', 'Genderless', 'unknown'];
   int? selectedStatus;
 
+  List<String> species = [
+    'Human',
+    'Humanoid',
+    'Alien',
+    'Robot',
+    'Animal',
+    'Croonenberg',
+    'Disease',
+    'Poopybutthole',
+    'Mythological Creature'
+  ];
+  int? selectedSpecies;
+
+  List<String> type = [
+    'Parasite',
+    'Human with giant head',
+    'Boobloosian',
+    'Elephant-Person',
+    'Superhuman',
+    'Dog',
+    'Bird-Person',
+    'Korblock',
+    'Gromflomite'
+        'Centaur',
+    'Organic gun',
+    'Microverse inhabitant',
+    'Vampire',
+    'Animal',
+    'Gromflomite',
+  ];
+  int? selectedType;
+
   List<String> selectedItem = [];
 
   @override
@@ -25,72 +58,111 @@ class _CharacterFilterState extends State<CharacterFilter> {
     return Container(
       height: 800,
       color: Colors.white,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 10),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                const Text(
-                  'Фильтрация',
-                  style: TextStyle(color: Colors.black, fontSize: 25),
-                ),
-                Material(
-                  child: IconButton(
-                    onPressed: () {
-                      context.router.pop();
-                    },
-                    icon: const Icon(
-                      Icons.close,
-                      size: 45,
-                    ),
+      child: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Text(
+                    'Filter',
+                    style: TextStyle(color: Colors.black, fontSize: 25),
                   ),
-                )
-              ],
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(left: 10, top: 20),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text('Gender'),
-                Row(
-                  children: genderChips(),
-                ),
-                Text('Status'),
-                Row(
-                  children: statusChips(),
-                )
-              ],
-            ),
-          ),
-          ElevatedButton(
-              onPressed: () {
-                context.router.pop();
-                context.read<CharacterBloc>().add(
-                      FilterCharacter(
-                        gender: selectedGender != null
-                            ? gender[selectedGender!]
-                            : '',
-                        status: selectedStatus != null
-                            ? status[selectedStatus!]
-                            : '',
+                  Material(
+                    child: IconButton(
+                      onPressed: () {
+                        context.router.pop();
+                      },
+                      icon: const Icon(
+                        Icons.close,
+                        size: 45,
                       ),
-                    );
-              },
-              child: const Text('Применить')),
-          ElevatedButton(
-              onPressed: () {
-                setState(() {
-                  selectedStatus = null;
-                  selectedGender = null;
-                });
-              },
-              child: const Text('Очистить'))
-        ],
+                    ),
+                  )
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 10, top: 20),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text(
+                    'Gender',
+                    style: TextStyle(fontSize: 18),
+                  ),
+                  Row(
+                    children: genderChips(),
+                  ),
+                  const SizedBox(
+                    height: 15,
+                  ),
+                  const Text('Status', style: TextStyle(fontSize: 18)),
+                  Row(
+                    children: statusChips(),
+                  ),
+                  const SizedBox(
+                    height: 15,
+                  ),
+                  const Text('Species', style: TextStyle(fontSize: 18)),
+                  Wrap(
+                    children: speciesChip(),
+                  ),
+                  const Text('Species', style: TextStyle(fontSize: 18)),
+                  Wrap(
+                    children: typeChip(),
+                  )
+                ],
+              ),
+            ),
+            Center(
+              child: Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+                child: Column(
+                  children: [
+                    PrimaryButton(
+                        onPressed: () {
+                          context.router.pop();
+                          context.read<CharacterBloc>().add(
+                                FilterCharacter(
+                                    gender: selectedGender != null
+                                        ? gender[selectedGender!]
+                                        : '',
+                                    status: selectedStatus != null
+                                        ? status[selectedStatus!]
+                                        : '',
+                                    species: selectedSpecies != null
+                                        ? species[selectedSpecies!]
+                                        : '',
+                                    type: selectedType != null
+                                        ? type[selectedType!]
+                                        : null),
+                              );
+                        },
+                        height: 50,
+                        title: 'Apply'),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    PrimaryButton(
+                        onPressed: () {
+                          setState(() {
+                            selectedStatus = null;
+                            selectedGender = null;
+                          });
+                        },
+                        height: 50,
+                        title: 'Clean')
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
@@ -132,6 +204,52 @@ class _CharacterFilterState extends State<CharacterFilter> {
           onSelected: (bool value) {
             setState(() {
               selectedStatus = i;
+            });
+          },
+        ),
+      );
+      chips.add(item);
+    }
+    return chips;
+  }
+
+  List<Widget> speciesChip() {
+    List<Widget> chips = [];
+    for (int i = 0; i < species.length; i++) {
+      Widget item = Padding(
+        padding: const EdgeInsets.only(left: 5, right: 5),
+        child: ChoiceChip(
+          label: Text(species[i]),
+          labelStyle: const TextStyle(color: Colors.white),
+          backgroundColor: Colors.blue,
+          selected: selectedSpecies == i,
+          selectedColor: Colors.black,
+          onSelected: (bool value) {
+            setState(() {
+              selectedSpecies = i;
+            });
+          },
+        ),
+      );
+      chips.add(item);
+    }
+    return chips;
+  }
+
+  List<Widget> typeChip() {
+    List<Widget> chips = [];
+    for (int i = 0; i < type.length; i++) {
+      Widget item = Padding(
+        padding: const EdgeInsets.only(left: 5, right: 5),
+        child: ChoiceChip(
+          label: Text(type[i]),
+          labelStyle: const TextStyle(color: Colors.white),
+          backgroundColor: Colors.blue,
+          selected: selectedType == i,
+          selectedColor: Colors.black,
+          onSelected: (bool value) {
+            setState(() {
+              selectedType = i;
             });
           },
         ),
